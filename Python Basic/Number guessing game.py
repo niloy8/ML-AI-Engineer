71bfa03bfb51514125f42eaaeed58b1af04a7e39
niloy8 <niloybhuiyan688@gmail.com>
2026-02-17 11:42:28 +0600

    Number guessing game build Functions Error handling Attempt counter Clean structure

---
+++ Python Basic/Number guessing game.py
@@ -0,0 +1,24 @@
+
+import random
+
+def number_guessing_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    print("Welcome to the Number Guessing Game!")
+
+    while True:
+        try:
+            guess = int(input("Enter the value between 1 and 100: "))
+            attempts += 1
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts!")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 100.")
+
+if __name__ == "__main__":
+    number_guessing_game()
